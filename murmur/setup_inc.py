0b99f803bbd49368db68259301828a467841ed82
KenjiTakahashi <wozniakk@gmail.com>
2013-06-20 03:44:50 +0200

    removed all graphical stuff

--- murmur/setup_inc.py
+++
@@ -1,8 +0,0 @@
-import re
-
-def getVersion():
-    versionfile = open('pymurmur/utils.py','r')
-    for line in versionfile.readlines():
-        line = line.strip()
-        if re.match(r'^version = ',line):
-            return line.split('=')[1].strip().replace('"','')
--- setup/setup.py
+++ setup/setup.py
@@ -10,8 +10,8 @@ setup(name                  = "museek-setup",
       author                = "daelstorm, hyriand",
       author_email          = "daelstorm@gmail.com",
       url                   = "http://museek-plus.org/",
-      long_description      = "musetup, musetup-gtk (config tools), and the unfinished musetup-qt",
-      scripts               = ['musetup', 'musetup-gtk', 'musetup-qt'],
-      data_files            = [('man/man1', ['musetup.1', 'musetup-gtk.1', 'musetup-qt.1'])],
+      long_description      = "musetup config tool",
+      scripts               = ['musetup'],
+      data_files            = [('man/man1', ['musetup.1'])],
 
 )
